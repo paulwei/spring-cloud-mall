0b5b90fe0d37506484dad130355a4a3730903f6e
weihonglin <weihonglin@shhxzq.com>
2022-07-17 18:59:11 +0800

    Initial spring cloud mall

---
+++ spring-cloud-histrix/src/main/java/com/whl/histrix/SpringCloudHistrixApplication.java
@@ -0,0 +1,13 @@
+package com.whl.histrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudHistrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudHistrixApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-histrix/src/test/java/com/whl/histrix/SpringCloudHistrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whl.histrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudHistrixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
